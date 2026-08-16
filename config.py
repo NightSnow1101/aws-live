e13020b8f5147d84a8d508e5cee25c94e8788ed8
NightSnow1101 <dicksonchong1101@gmail.com>
2026-08-17 00:09:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.cf4miet7fohr.us-east-1.rds.amazonaws.com"
 customuser = "aws_server"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "dicksonchong-employee1"
+custombucket = "dicksonchong-employee"
 customregion = "us-east-1"
 
