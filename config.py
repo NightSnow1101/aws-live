fd2a7c8c2509ee04930a630ac44aa2970d9cda3a
NightSnow1101 <dicksonchong1101@gmail.com>
2026-08-17 00:02:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cf4miet7fohr.us-east-1.rds.amazonaws.com"
+customuser = "aws_server"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "dicksonchong-employee1"
+customregion = "us-east-1"
+
